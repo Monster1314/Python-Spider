7e9faa8654dfde4418b7b11c613df0913d03e881
MuZhaoyang777 <67379035+MuZhaoyang777@users.noreply.github.com>
2020-11-08 10:08:22 +0000

    Update requests请求猫眼.py

--- Class 2 请求与响应/requests请求猫眼.py
+++ Class 2 请求与响应/requests请求猫眼.py
@@ -1,3 +1,12 @@
+"""
+    目标地址：https://maoyan.com/board/4?offset=0
+
+    要求：
+    1、请求到目标网址数据，需要在请求到的数据中看到当前页面所有的电影名字、主演、上映时间、评分等信息
+    2、请列举在请求不到数据时，需要添加几个常见请求头字段（课程讲过）
+    请在下方编写代码
+"""
+
 import requests
 import re
 
@@ -11,7 +20,7 @@ headers = {
 
 response = requests.get(url=url, headers=headers)
 response.encoding = response.apparent_encoding
-# print(response.text)
+
 
 names = re.findall('<p class="name"><a href=".*?" title="(.*?)" data-act="boarditem-click" data-val=".*?">.*?</a></p>',
                    response.text, re.S)
@@ -20,7 +29,6 @@ release_times = re.findall('<p class="releasetime">(.*?)</p>', response.text, re
 scores = re.findall('<p class="score"><i class="integer">(.*?)</i><i class="fraction">(.*?)</i></p>',
                     response.text, re.S)
 splice_score = [x + y for x, y in scores]
-# print(names, stars, release_times, scores)
 
 for i in range(len(names)):
     print(names[i], stars[i].strip(), release_times[i], splice_score[i])
