79e4229ce9d3fa91e8dc4ab6050cb538f6e0b300
MuZhaoyang777 <67379035+MuZhaoyang777@users.noreply.github.com>
2020-11-08 10:07:56 +0000

    Update json字段获取.py

--- Class 2 请求与响应/json字段获取.py
+++ Class 2 请求与响应/json字段获取.py
@@ -1,12 +1,21 @@
+"""
+    目标网址：https://www.ku6.com/video/feed?pageNo=0&pageSize=40&subjectId=76
+
+    要求：
+        1、请求上述网址的数据
+        2、按照要求提取以下字段信息
+            title、
+            picPath、
+            playUrl
+        提取下来用 print() 函数打印即可
+请在下方编写代码
+"""
 import requests
-import pprint
 
 url = 'https://www.ku6.com/video/feed?pageNo=0&pageSize=40&subjectId=76'
 response = requests.get(url=url)
 response.encoding = response.apparent_encoding
 data = response.json()['data']
-# print(type(data[0]))
-# pprint.pprint(response.json()['data'])
 
 for i in range(len(data)):
     print('title:', data[i]['title'], 'picPath:', data[i]['picPath'], 'playUrl:', data[i]['playUrl'])
