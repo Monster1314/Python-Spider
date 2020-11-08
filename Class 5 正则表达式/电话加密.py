6c03d9b1c3540a3e1b33b5ebab62fa2a044e1539
MuZhaoyang777 <67379035+MuZhaoyang777@users.noreply.github.com>
2020-11-08 09:51:40 +0000

    Create 电话加密.py

---
+++ Class 5 正则表达式/电话加密.py
@@ -0,0 +1,14 @@
+import re
+
+
+tel = "18123115458"
+
+def func(temp):
+    print(temp)
+    str_ = temp.group(0)
+    print(str_)
+    return str_[:3] + '****' + str_[-4:]
+
+# func 是sub方法会默认调用的函数, return返回的结果就是最终替换的结果
+result = re.sub('\d{11}', func, tel, re.S)
+print(result)
