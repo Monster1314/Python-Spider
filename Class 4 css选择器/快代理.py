aaf422fbb19f9f655ed0fbbfcc459ae249f993ba
MuZhaoyang777 <67379035+MuZhaoyang777@users.noreply.github.com>
2020-11-08 10:05:06 +0000

    Update 快代理.py

--- Class 4 css选择器/快代理.py
+++ Class 4 css选择器/快代理.py
@@ -1,3 +1,16 @@
+"""
+    使用 css 选择器将快代理中我需要的信息提取出来。
+    目标网址：https://www.kuaidaili.com/free/
+    
+    需要解析以下数据:
+        ip、
+        port、
+        类型
+
+    提取出来print（）打印即可
+    温馨提示: 爬取速度不要太快,加延迟,避免被封导致以后请求不到数据!
+"""
+
 import requests
 import parsel
 import time
